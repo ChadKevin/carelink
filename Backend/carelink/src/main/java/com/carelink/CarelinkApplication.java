aa2673fea918c15aebeb8ddda6c1cfb5aac0d2b4
ChadKevin <girindrajaiswal230@gmail.com>
2026-08-25 10:09:30 +0530

    carelink

--- Backend/carelink/src/main/java/com/carelink/CarelinkApplication.java
+++ Backend/carelink/src/main/java/com/carelink/CarelinkApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.carelink;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class CarelinkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CarelinkApplication.class, args);
 	}
 
 }
--- Backend/carelink/src/test/java/com/carelink/CarelinkApplicationTests.java
+++ Backend/carelink/src/test/java/com/carelink/CarelinkApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.carelink;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class CarelinkApplicationTests {
 
 	@Test
 	void contextLoads() {
